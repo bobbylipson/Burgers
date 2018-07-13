545f8726fcc4386aade2f04c40096be624766e72
BobbyL <robertlipson2012@gmail.com>
2018-07-13 18:08:29 -0500

    app complete

---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+###seeds
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bobs Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Good Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger', false);
